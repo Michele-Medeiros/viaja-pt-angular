156b9acdb2fca2f7b8c0dfae70acafef9cbc81a0
Michele-Medeiros <99910808+Michele-Medeiros@users.noreply.github.com>
2023-06-13 19:07:02 +0100

    Trabalho inicialmente funcionando

---
+++ micheleMedeiros_PropostaII/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { AboutComponent } from './components/body/about/about.component';
+import { DestinationsComponent } from './components/body/destinations/destinations.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    NavbarComponent,
+    AboutComponent,
+    DestinationsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
