42b6b466540af5a5f304e265c178413b7d7bb5c1
Michele-Medeiros <99910808+Michele-Medeiros@users.noreply.github.com>
2023-06-15 14:10:25 +0100

    Trabalho  99% finalizado
    Falta apenas fazer o relatório

--- micheleMedeiros_PropostaII/src/app/app.module.ts
+++ micheleMedeiros_PropostaII/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,7 +10,6 @@ import { NavbarComponent } from './components/header/navbar/navbar.component';
 import { AboutComponent } from './components/body/about/about.component';
 import { DestinationsComponent } from './components/body/destinations/destinations.component';
 import { TestimonyComponent } from './components/body/testimony/testimony.component';
-import { AlunosComponent } from './components/alunos/alunos.component';
 
 
 @NgModule({
@@ -22,11 +22,10 @@ import { AlunosComponent } from './components/alunos/alunos.component';
     AboutComponent,
     DestinationsComponent,
     TestimonyComponent,
-    AlunosComponent,
-
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- micheleMedeiros_PropostaII/src/app/components/footer/footer.component.ts
+++ micheleMedeiros_PropostaII/src/app/components/footer/footer.component.ts
@@ -14,12 +14,12 @@ export class FooterComponent {
     this.grupo = {
       nomeAluno1: 'Michele Medeiros',
       numeroAluno1: 'al80395',
-      turmaAluno1: 'Turma 2',
+      turmaAluno1: '2',
       emailAluno1: 'michelefmedeiros@gmail.com',
       nomeAluno2: 'Rafael Costa ',
       numeroAluno2: 'al80076 ',
-      turmaAluno2: 'Turma 2',
-      emailAluno2: 'rafael@example.com'
+      turmaAluno2: '2',
+      emailAluno2: 'rafael20032003@outlook.com'
     };
   }
 }
